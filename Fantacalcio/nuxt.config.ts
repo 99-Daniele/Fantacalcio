8b1b58b646ce80ea95d26cbacde16a70a86d5742
Daniele Cicala <10630561@polimi.it>
2023-07-24 10:28:05 +0200

    project build

--- Fantacalcio/nuxt.config.ts
+++ Fantacalcio/nuxt.config.ts
@@ -1,4 +1,42 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  css: [],
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
+  modules: ['@nuxtjs/supabase'],
+  ssr: true,
+  app: {
+    head: {
+      title: 'Fantacalcio',
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+      meta: [
+        {
+          charset: 'UTF-8',
+        },
+        {
+          name: 'description',
+          content:
+            'Gioca al Fantacalcio, il gioco piu amato dagli italiani.',
+        },
+        {
+          name: 'keywords',
+          content:
+            'fantacalcio, calcio, football, italia, serie a',
+        },
+        {
+          name: 'author',
+          content:
+            'Daniele Cicala',
+        },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1.0',
+        },
+      ],
+    },
+  },
+});
